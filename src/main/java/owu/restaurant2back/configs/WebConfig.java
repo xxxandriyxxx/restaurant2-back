41088d23044e1ed4e0564a4d9f9c2798f34117f8
Andriy <asoha474@gmail.com>
2019-08-20 01:00:20 +0300

    changed the URL of front-end project

--- src/main/java/owu/restaurant2back/configs/WebConfig.java
+++ src/main/java/owu/restaurant2back/configs/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     // URL of the running front-end project
     // it used in EmailServiceImpl class
-    public static final String url = "http://localhost:4200";
-//    public static final String url = "http://localhost:8080";
+//    public static final String url = "http://localhost:4200";
+    public static final String url = "http://localhost:8080";
 //    public static final String url = "http://ec2-18-217-248-193.us-east-2.compute.amazonaws.com:8080";
 
     // path to the logos of restaurants
